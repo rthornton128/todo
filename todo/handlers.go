d6aad0ee19c5e0369ea460462653cd46f9d22b63
Rob Thornton <rthornton128@gmail.com>
2016-07-11 22:40:18 -0700

    implement handlers

--- todo/handlers.go
+++ todo/handlers.go
@@ -2,23 +2,76 @@
 // Governed by a Simplified BSD license.  See the LICENSE file or you may
 // find a copy at: http://www.freebsd.org/copyright/freebsd-license.html
 
+// Package serve contains the HTTP handlers for Todo. It also provides
+// an errorHandler middleware wrapper.
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
 
-func errHandler(f func(w http.ResponseWriter, r *http.Response) error) http.HandlerFunc {
-	return nil
+	"github.com/gorilla/mux"
+	"github.com/rthornton128/todo/task"
+)
+
+func errHandler(f func(w http.ResponseWriter, r *http.Request) error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if err := f(w, r); err != nil {
+			switch err.(type) {
+			// database error = StatusInternalServerError
+			// badRequest
+			// ??
+			}
+		}
+		// send StatusOk
+	}
 }
 
-func handleNewTask(w http.ResponseWriter, r *http.Response) error {
-	return nil
+func handleNewTask(w http.ResponseWriter, r *http.Request) error {
+	var t task.Task
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		return err
+	}
+	return m.Store(&t)
 }
-func handleAllTasks(w http.ResponseWriter, r *http.Response) error {
-	return nil
+
+func handleAllTasks(w http.ResponseWriter, r *http.Request) error {
+	all, err := m.All()
+	if err != nil {
+		return err
+	}
+	return json.NewEncoder(w).Encode(all)
 }
-func handleQueryTask(w http.ResponseWriter, r *http.Response) error {
-	return nil
+
+func handleQueryTask(w http.ResponseWriter, r *http.Request) error {
+	tmp := mux.Vars(r)["id"]
+	id, err := strconv.ParseInt(tmp, 10, 64)
+	if err != nil {
+		return err
+	}
+	t, err := m.Query(id)
+	if err != nil {
+		return err
+	}
+	return json.NewEncoder(w).Encode(t)
 }
-func handleUpdateTask(w http.ResponseWriter, r *http.Response) error {
-	return nil
+
+func handleUpdateTask(w http.ResponseWriter, r *http.Request) error {
+	tmp := mux.Vars(r)["id"]
+	id, err := strconv.ParseInt(tmp, 10, 64)
+	if err != nil {
+		return err
+	}
+	var t task.Task
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+		return err
+	}
+	if t.ID != id {
+		return errors.New("body and URI ID do not match")
+	}
+	return m.Store(&t)
 }
--- todo/todo.go
+++ todo/todo.go
@@ -10,22 +10,30 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/rthornton128/todo/task"
 )
 
 var addr = "localhost:8080"
+var m *task.Manager
 
 func main() {
 	// init handlers
 	r := mux.NewRouter()
-	r.Handle("/", http.FileServer(http.Dir("/static")))
+	r.Handle("/", http.FileServer(http.Dir("/static/")))
 	r.HandleFunc("/tasks", errHandler(handleAllTasks)).Methods("GET")
 	r.HandleFunc("/tasks", errHandler(handleNewTask)).Methods("POST")
 	r.HandleFunc("/tasks/{id}", errHandler(handleQueryTask)).Methods("GET")
 	r.HandleFunc("/tasks/{id}", errHandler(handleUpdateTask)).Methods("PUT")
 	//http.Handle(r)
 
+	var err error
+	m, err = task.NewManager(":memory:") // TODO allow for user defined database
+	if err != nil {
+		log.Fatal("init task manager:", err)
+	}
+
 	// start server
-	err := http.ListenAndServe(addr, r)
+	err = http.ListenAndServe(addr, r)
 	if err != nil {
 		log.Fatalf("listen and serve: %v", err)
 	}
